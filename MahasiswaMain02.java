be6649b5e19cb70004c6d304ec591583b0f8ec70
Andini <163494512+Threeguana@users.noreply.github.com>
2025-02-19 08:05:41 +0700

    Update MahasiswaMain02.java

--- MahasiswaMain02.java
+++ MahasiswaMain02.java
@@ -1,5 +1,7 @@
 public class MahasiswaMain02 {
     public static void main(String[] args) {
+        Mahasiswa02 mhsAndini = new Mahasiswa02("Andini", "244107060140", "SIB 1E", 3.80);
+
         Mahasiswa02 mhs1 = new Mahasiswa02();
         
         mhs1.nama = "Muhammad Ali Farhan";
@@ -11,5 +13,10 @@ public class MahasiswaMain02 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIPK(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa02 mhs2 = new Mahasiswa02("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+
+        mhs2.updateIPK(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
